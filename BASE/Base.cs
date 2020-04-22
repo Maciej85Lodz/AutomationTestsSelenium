c56826ef85ba0f471ac341347d11177d669b0620
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-22 22:46:19 +0200

    Page Navigation without Generics.

---
+++ BASE/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.BASE
+{
+    public class Base
+    {
+
+        public BasePage CurrentPage { get; set; }
+
+    }
+}
