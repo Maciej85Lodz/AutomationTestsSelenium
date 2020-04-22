c45c093b69feebdbb4cc1056214774b2dfb7c928
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-22 23:13:39 +0200

    Developing Base Component; Page Navigation with Generics in C#;

--- BASE/Base.cs
+++ BASE/Base.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
 
 namespace AutomationFramework.BASE
 {
     public class Base
     {
-
+        private IWebDriver _driver { get; set; }
         public BasePage CurrentPage { get; set; }
+
+        protected TPage GetInstance<TPage>() where TPage : BasePage, new()
+        {
+            TPage pageInstance = new TPage()
+            {
+                _driver = DriverContext.Driver
+            };
+
+            PageFactory.InitElements(DriverContext.Driver, pageInstance);
+
+            return pageInstance;
+        }
+
+        public TPage As<TPage>() where TPage : BasePage
+        {
+            return (TPage)this;
+        }
 
     }
 }
