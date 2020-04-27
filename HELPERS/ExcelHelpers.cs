408a2af0cbaa62506f9490f39a7c58753265d518
maciej.wolejszo <maciej.wolejszo@gmail.com>
2020-04-27 15:47:25 +0200

    Developing Helpers component; Excel Helper;

--- HELPERS/ExcelHelpers.cs
+++ HELPERS/ExcelHelpers.cs
@@ -1,12 +1,89 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace AutomationFramework.HELPERS
 {
-    class ExcelHelpers
+    public class ExcelHelpers
     {
+        private static List<Datacollection> _dataCol = new List<Datacollection>();
+
+        /// <summary>
+        /// Storing all the excel values in to the in-memory collections
+        /// </summary>
+        /// <param name="fileName"></param>
+        public static void PopulateInCollection(string fileName)
+        {
+            DataTable table = ExcelToDataTable(fileName);
+            for (int row = 1; row <= table.Rows.Count; row++)
+            {
+                for (int col = 0; col < table.Columns.Count; col++)
+                {
+                    Datacollection dtTable = new Datacollection()
+                    {
+                        rowNumber = row,
+                        colName = table.Columns[col].ColumnName,
+                        colValue = table.Rows[row - 1][col].ToString()
+                    };
+                    _dataCol.Add(dtTable);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Reading all the data from Excel Sheet
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        
+        private static DataTable ExcelToDataTable(string fileName)
+        {
+            using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (data) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+
+                    DataTableCollection table = result.Tables;
+                    DataTable resultTable = table["Sheet1"];
+                    return resultTable;
+                }
+            }
+        }
+
+        public static string ReadData(int rowNumber, string columnName)
+        {
+            try
+            {
+                //Retriving Data using LINQ to reduce too many iterations
+                string data = (from colData in _dataCol
+                               where colData.colName == columnName && colData.rowNumber == rowNumber
+                    select colData.colValue).SingleOrDefault();
+
+                return data.ToString();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+     
+        public class Datacollection
+        {
+            public int rowNumber { get; set; }
+            public string colName { get; set; }
+            public string colValue { get; set; }
+        }
     }
 }
